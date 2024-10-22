f5294a366b8bb088d67bb476b6068307863515a7
MaiconDequicy <maiconcosta@icomp.ufam.ufam.edu.br>
2024-10-22 14:20:13 -0400

    tela de apresentação

---
+++ app/src/main/java/br/pagehub/SplashScreen.kt
@@ -0,0 +1,71 @@
+package br.pagehub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.pagehub.ui.theme.PageHubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PageHubTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = Color(0xFF00BFA5)
+                ) { innerPadding ->
+                    MainContent(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFF00BFA5)) // Define a cor de fundo do Box, que ocupa toda a tela
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp), // Adiciona padding ao redor
+            verticalArrangement = Arrangement.Center, // Centraliza verticalmente
+            horizontalAlignment = Alignment.CenterHorizontally // Centraliza horizontalmente
+        ) {
+            Text(
+                text = "Page Hub", // // Nome do app
+                style = MaterialTheme.typography.headlineLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 42.sp,
+                    ),
+                color = Color.White
+                )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PageHubTheme {
+        MainContent()
+    }
+}
